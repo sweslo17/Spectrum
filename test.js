a5fc5bc562a950f25654f7b7be0c89bda88922f1
sweslo17 <sweslo17@gmail.com>
2014-03-27 01:32:48 +0800

    still parsererror

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-/*chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-	  alert(tabs[0].url);
-   }
-);*/
-//console.log($('title').text());
-//console.log(window.location.href);
-var url = "https://spreadsheets.google.com/feeds/cells/1yapaaaFn0mtJF0CMiIil4Y1uYCqS97jIWL4iBZMPAKo/od6/public/values?alt=json-in-script&callback=?";
-$.getJSON(url,{}, function (d) { $('body').append(d); });
+var url = "https://spreadsheets.google.com/feeds/cells/1yapaaaFn0mtJF0CMiIil4Y1uYCqS97jIWL4iBZMPAKo/od6/public/values?alt=json-in-script&callback=myCallback";
+//alert('test');
+$.get(url,{}, function (d) { alert(d);}).done(function() {
+    alert( "second success" );
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log( textStatus );
+  })
+  .always(function() {
+    alert( "finished" );
+  });;
