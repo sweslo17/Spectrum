78391f24c389ad2156af061b25317c2324e82fa6
sweslo17 <sweslo17@gmail.com>
2014-03-28 08:37:41 +0800

    runable test.js

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
+$().ready(function(){
 var url = "https://spreadsheets.google.com/feeds/cells/1yapaaaFn0mtJF0CMiIil4Y1uYCqS97jIWL4iBZMPAKo/od6/public/values?alt=json-in-script&callback=myCallback";
 //alert('test');
-$.get(url,{}, function (d) { alert(d);}).done(function() {
-    alert( "second success" );
+/*$.ajax(url,dataType: jsonp).done(function() {
+    console.log( "second success" );
   })
   .fail(function(jqXHR, textStatus, errorThrown) {
     console.log( textStatus );
   })
   .always(function() {
-    alert( "finished" );
-  });;
+    console.log( "finished" );
+  });*/
+  var url = 'https://spreadsheets.google.com/feeds/cells/1yapaaaFn0mtJF0CMiIil4Y1uYCqS97jIWL4iBZMPAKo/od6/public/values?alt=json-in-script&callback=?';
+	jQuery.getJSON(url).success(function(data) {
+		console.log(data); 
+	}).error(function(message) {
+		console.error('error' + message); 
+	}).complete(function() {
+		console.log('completed!'); 
+	});
+});
