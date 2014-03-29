232e1ca9eaebed737f3c37ff368e1b7df96a4c9e
sweslo17 <sweslo17@gmail.com>
2014-03-29 19:30:43 +0800

    Update test.js

--- test.js
+++ test.js
@@ -10,24 +10,45 @@ $().ready(function(){
   .always(function() {
     console.log( "finished" );
   });*/
-  var url = 'https://spreadsheets.google.com/tq?tqx=&tq=SELECT%20A%2CB%20WHERE%20B%3D%27http%3A%2F%2Fwww.ettoday.net%2Fnews%2F20140327%2F339912.htm%27&key=0AmNbDWfyHFBMdENXT1hfVl9BN1BuSUl3Q2NkelIwZmc&callback=myCallback';
-	jQuery.ajax({
-        url : url,
-        type : 'GET',
-        dataType : 'html',
-        error: function(xhr, status, error) {
-          var err = eval("(" + xhr.responseText + ")");
-          alert(err.Message);
-        },
-        success: function(data){
-            console.log(JSON.parse(data.split("(").pop().split(")")[0])); 
+    var search_url;
+    chrome.tabs.getSelected(null,function(tab) {
+        search_url = tab.url;
+        console.log(encodeURIComponent(search_url));
+        var url = 'https://spreadsheets.google.com/tq?tqx=&key=0AmNbDWfyHFBMdENXT1hfVl9BN1BuSUl3Q2NkelIwZmc&tq=SELECT%20A%2CB%20WHERE%20B%3D%27'+encodeURIComponent(search_url)+'%27';
+        console.log(url);
+        var result;
+        jQuery.ajax({
+            url : url,
+            type : 'GET',
+            dataType : 'html',
+            async : false,
+            error: function(xhr, status, error) {
+              var err = eval("(" + xhr.responseText + ")");
+              alert(err.Message);
+            },
+            success: function(data){
+                console.log(data.substring(data.indexOf("(")+1,data.lastIndexOf(")"))); 
+                result = JSON.parse(data.substring(data.indexOf("(")+1,data.lastIndexOf(")")));
+                console.log(result);
+            }
+        });
+        
+        if(result.table.rows.length!=0)
+        {
+            $('body').text(result.table.rows[0].c[0].v);
+                //console.log(tablink);
+        }
+        else
+        {
+            $('body').text('not found');
         }
     });
+
     /*.success(function(data) {
-		console.log(data); 
-	}).error(function(message) {
-		console.log('errorrrr' + message); 
-	}).complete(function() {
-		console.log('completed!'); 
-	});*/
+        console.log(data); 
+    }).error(function(message) {
+        console.log('errorrrr' + message); 
+    }).complete(function() {
+        console.log('completed!'); 
+    });*/
 });
