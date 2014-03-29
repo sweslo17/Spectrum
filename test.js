17b661260b0054b0074bbf23af17dc925b51f968
sweslo17 <sweslo17@gmail.com>
2014-03-30 01:21:08 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,14 @@
 $().ready(function(){
-//var url = "https://spreadsheets.google.com/feeds/cells/1yapaaaFn0mtJF0CMiIil4Y1uYCqS97jIWL4iBZMPAKo/od6/public/values?alt=json-in-script&callback=myCallback";
-//alert('test');
-/*$.ajax(url,dataType: jsonp).done(function() {
-    console.log( "second success" );
-  })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    console.log( textStatus );
-  })
-  .always(function() {
-    console.log( "finished" );
-  });*/
     var search_url;
     chrome.tabs.getSelected(null,function(tab) {
+        console.log(tab);
         search_url = tab.url;
+        search_title = tab.title;
         console.log(encodeURIComponent(search_url));
-        var url = 'https://spreadsheets.google.com/tq?tqx=&key=0AmNbDWfyHFBMdENXT1hfVl9BN1BuSUl3Q2NkelIwZmc&tq=SELECT%20A%2CB%20WHERE%20B%3D%27'+encodeURIComponent(search_url)+'%27';
-        console.log(url);
+        var url = 'https://spreadsheets.google.com/tq?tqx=&key=0AmNbDWfyHFBMdENXT1hfVl9BN1BuSUl3Q2NkelIwZmc&tq=';
+        //(B='"+search_url+"') AND (
+        var query = "SELECT C,D WHERE (B='"+search_url+"') AND (E=1)";
+        console.log(url = url+encodeURIComponent(query));
         var result;
         jQuery.ajax({
             url : url,
@@ -35,7 +28,22 @@ $().ready(function(){
         
         if(result.table.rows.length!=0)
         {
-            $('body').text(result.table.rows[0].c[0].v);
+            var str = '<p>此篇是「<b>' + result.table.rows[0].c[1].v + '」議題之<i>';
+            if(result.table.rows[0].c[0].v > 0)
+            {
+                str += '<font color="green">贊同</font>';
+            }
+            else if(result.table.rows[0].c[0].v < 0)
+            {
+                str += '<font color="red">反對</font>';
+            }
+            else if(result.table.rows[0].c[0].v == 0)
+            {
+                str += '<font color="grey">中立</font>';
+            }
+            str += '</i>論述</p>';
+            str += '<p>強度為'+result.table.rows[0].c[0].v+'分</p>';
+            $('body').append(str);
                 //console.log(tablink);
         }
         else
@@ -43,12 +51,4 @@ $().ready(function(){
             $('body').text('not found');
         }
     });
-
-    /*.success(function(data) {
-        console.log(data); 
-    }).error(function(message) {
-        console.log('errorrrr' + message); 
-    }).complete(function() {
-        console.log('completed!'); 
-    });*/
 });
